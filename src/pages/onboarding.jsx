8013b19efbe2ed99a3d575a4b3e13d22ab15ad76
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-02-06 23:41:03 +0530

    Job Listing Page and search,filter and others functionality are added

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,12 +1,11 @@
 import { useUser } from "@clerk/clerk-react";
-import { BarLoader } from "react-spinners";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { BarLoader } from "react-spinners";
 
 const Onboarding = () => {
   const { user, isLoaded } = useUser();
-
   const navigate = useNavigate();
 
   const navigateUser = (currRole) => {
@@ -34,6 +33,7 @@ const Onboarding = () => {
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
+
   return (
     <div className="flex flex-col items-center justify-center mt-40">
       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">
